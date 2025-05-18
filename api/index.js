9a20d61a16cf0a0e3bf125a0b33f9bac3be27265
abhishekhojha <abhishekhojha45@gmail.com>
2025-05-18 23:48:47 +0530

    fix: update import path for User model and adjust Google sign-in link in Login component

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const DB = require("./config/DB");
+require('./config/passport');
+
 const port = process.env.PORT || 4000;
 app.use(cors());
 app.use(express.json());
